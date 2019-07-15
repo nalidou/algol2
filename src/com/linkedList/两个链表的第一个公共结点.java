643b5ce412a715214340361edb471e30717402a0
wuzhy <wuzhy@rd.netease.com>
2019-07-15 11:04:19 +0800

    MOD:debug

--- src/com/linkedList/两个链表的第一个公共结点.java
+++ src/com/linkedList/两个链表的第一个公共结点.java
@@ -13,7 +13,7 @@ public class 两个链表的第一个公共结点 {
         LinkedListUtil.LNode head1 = LinkedListUtil.createLNodeList(new int[]{1,2,3,4,5,6});
         LinkedListUtil.LNode head2 = LinkedListUtil.createLNodeList(new int[]{1,2,3,4,5,6,7,8,9});
         getPublicNode(head1, head2);
-
+        LinkedListUtil.forEachLNode(head1);
     }
 
     static LinkedListUtil.LNode getPublicNode(LinkedListUtil.LNode head1, LinkedListUtil.LNode head2){
