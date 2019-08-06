f95a9377d44b4d3f59da335e1c5d43ba94f26510
wuzhy <wuzhy@rd.netease.com>
2019-08-06 17:33:01 +0800

    MOD:更新

--- src/com/biTree/树的子结构.java
+++ src/com/biTree/树的子结构.java
@@ -1,5 +1,10 @@
 package com.biTree;
 
+import com.util.BiTreeUtil;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 来源：剑指offer 17
  * 输入两颗二叉树A，B，判断B是不是A的子结构。
@@ -7,5 +12,38 @@ package com.biTree;
  */
 public class 树的子结构 {
 
+    public static void main(String[] args) {
+
+        String [] pre = new String[] {"1", "2", "4", "7", "3", "5", "6", "8"};
+        String [] mid = new String[] {"4", "7", "2", "1", "5", "3", "8", "6"};
+
+        List<String> preList = Arrays.asList(pre);
+        List<String> midList = Arrays.asList(mid);
+        BiTreeUtil.BiTreeNode root = BiTreeUtil.createTree(preList, midList);
+        BiTreeUtil.forEach(root);
+    }
+
+    static boolean isChildTree(BiTreeUtil.BiTreeNode root, BiTreeUtil.BiTreeNode child) {
+        if (root.v.equals(child.v)) {
+            return isEquals(root, child);
+        } else {
+            boolean isLChild = isChildTree(root.l, child);
+            boolean isRChild = isChildTree(root.r, child);
+            return (isLChild | isRChild);
+        }
+
+    }
+    static boolean isEquals(BiTreeUtil.BiTreeNode root, BiTreeUtil.BiTreeNode child){
+        if (child == null) {
+            return true;
+        }
+        if (root == null) {
+            return false;
+        }
+        if (!root.v.equals(child.v)) {
+            return false;
+        }
+        return isEquals(root.l, child.l) && isEquals(root.r, child.r);
+    }
 
 }
